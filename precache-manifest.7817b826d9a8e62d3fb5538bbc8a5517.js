4a6a0629201a67b96af3e61dce7976f24b2ffad2
yshay <yshay@cuidas.com.br>
2020-03-27 15:36:35 -0300

    Updates

---
+++ precache-manifest.7817b826d9a8e62d3fb5538bbc8a5517.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230e12798247956eac54bc154160c5dc",
+    "url": "/park-pass-app/index.html"
+  },
+  {
+    "revision": "4418b37ae7962dd0c1e2",
+    "url": "/park-pass-app/static/css/main.ba2fd85b.chunk.css"
+  },
+  {
+    "revision": "ef6309457c432c9a3b97",
+    "url": "/park-pass-app/static/js/2.a6e26d2c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/park-pass-app/static/js/2.a6e26d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4418b37ae7962dd0c1e2",
+    "url": "/park-pass-app/static/js/main.e4e45144.chunk.js"
+  },
+  {
+    "revision": "5897fe72243b2ec6aeaa",
+    "url": "/park-pass-app/static/js/runtime-main.1a495881.js"
+  },
+  {
+    "revision": "53cec34450306311182370dc9d51293b",
+    "url": "/park-pass-app/static/media/burger.53cec344.svg"
+  },
+  {
+    "revision": "1a91c1735e951c7c8ffda04e1c453afd",
+    "url": "/park-pass-app/static/media/close.1a91c173.svg"
+  }
+]);
